48245c04f0110502548be2cac009e318db7ead71
PalomaBlanca <paul.deg37@gmail.com>
2020-01-17 21:42:20 +0100

    added Form for "einsteigen" and "finden"

---
+++ frontend/src/components/molecules/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button :type="type">
+        <slot>
+        </slot>
+    </button>
+</template>
+<script>
+export default {
+    name: 'Button',
+    props: {
+        type: ''
+    },
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
--- frontend/src/components/organisms/Form.vue
+++ frontend/src/components/organisms/Form.vue
@@ -1,13 +1,17 @@
 <template>
     <div>
-        <button>EINSTEIGEN</button>
-        <button>FINDEN</button>
+        <form action="/"><input type="text" placeholder="In welchen Zug steigst du ein?" required/><Button type="submit">EINSTEIGEN</Button></form><br/>
+        <form action="/"><input type="text" placeholder="Welchen Zug suchst du?" required/><Button type="submit">FINDEN</Button></form>
     </div>
 </template>
 
 <script>
+import Button from '../molecules/Button'
 export default {
-    
+    name: 'Form',
+    components: {
+        Button,
+    }
 }
 </script>
 
