403c185b48b2d9b7adba4585599d0fbc7c23cfe1
PalomaBlanca <paul.deg37@gmail.com>
2020-01-18 13:29:17 +0100

    deleted <br/>

--- frontend/src/components/organisms/Form.vue
+++ frontend/src/components/organisms/Form.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <form action="/"><input type="text" placeholder="In welchen Zug steigst du ein?" required/><Button type="submit">EINSTEIGEN</Button></form><br/>
+        <form action="/"><input type="text" placeholder="In welchen Zug steigst du ein?" required/><Button type="submit">EINSTEIGEN</Button></form>
         <form action="/"><input type="text" placeholder="Welchen Zug suchst du?" required/><Button type="submit">FINDEN</Button></form>
     </div>
 </template>
