7530b09ccaad538f1b04de57f95b4860d0e89570
PalomaBlanca <paul.deg37@gmail.com>
2020-05-26 23:23:37 +0200

    added basic colors

--- frontend/src/components/organisms/Header.vue
+++ frontend/src/components/organisms/Header.vue
@@ -1,22 +1,20 @@
 <template>
     <header class="mb-5 mt-3 container-fluid">
-        <div class="row">​
-            <picture class="col-12 text-center">
+        <div class="row justify-content-center">​
+            <picture class="col-12 text-center w-100">
                 <source srcset="https://wimz.de.cool/wp-content/uploads/2020/05/logo-2-e1588342746322.png" media="(min-width: 300px)">
                 <source srcset="https://wimz.de.cool/wp-content/uploads/2020/05/logo-e1588342629493.png" media="(min-width: 600px)">
                 <source srcset="https://wimz.de.cool/wp-content/uploads/2020/05/logo-1.png" media="(min-width: 3000px)">
                 <img src="logo.png" alt="MDN">
             </picture>
-        </div>
-        <div class="row mt-2">
-            <p class="text-center w-100">Reisende helfen Reisenden.</p>
+            <p class="col-12 w-100">Reisende helfen Reisenden.</p>
         </div>
     </header>
 </template>
 
 <script>
 export default {
-    
+
 }
 </script>
 
